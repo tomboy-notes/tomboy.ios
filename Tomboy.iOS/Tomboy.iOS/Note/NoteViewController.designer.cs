7792b9e1d0da2c104dd78a5ad427cea2fe260304
Rashid Khan <rashood.khan@gmail.com>
2014-07-01 01:58:39 +0400

    Added Outlets to WebView and Title Bar in Note Taking Screen

--- Tomboy.iOS/Tomboy.iOS/Note/NoteViewController.designer.cs
+++ Tomboy.iOS/Tomboy.iOS/Note/NoteViewController.designer.cs
@@ -1,12 +1,10 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
-using System;
 using MonoTouch.Foundation;
-using MonoTouch.UIKit;
 using System.CodeDom.Compiler;
 
 namespace Tomboy.iOS
@@ -14,8 +12,34 @@ namespace Tomboy.iOS
 	[Register ("NoteViewController")]
 	partial class NoteViewController
 	{
+		[Outlet]
+		MonoTouch.UIKit.UITextField[] NoteTitle { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITextField Title { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIWebView WebView { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIWebView WebViewer { get; set; }
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (WebView != null) {
+				WebView.Dispose ();
+				WebView = null;
+			}
+
+			if (Title != null) {
+				Title.Dispose ();
+				Title = null;
+			}
+
+			if (WebViewer != null) {
+				WebViewer.Dispose ();
+				WebViewer = null;
+			}
 		}
 	}
 }
